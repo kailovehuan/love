f6579ab17e6d92350431b0850bdcaab08160ec3a
syh03 <1336840590@qq.com>
2017-03-24 18:48:18 +0800

    Grab

--- Grab_WebData/grab_try.py
+++ Grab_WebData/grab_try.py
@@ -5,3 +5,17 @@ Created on 2016/6/13
 @author: sunyihuan
 '''
 from bs4 import BeautifulSoup
+import requests
+import lxml
+
+soup = open("adposter_edit.html", 'r')
+soup = BeautifulSoup(open('adposter_edit.html'), 'lxml')
+head = soup.head.link
+tag = soup.div
+
+css_soup = BeautifulSoup('<p class="body strikeout" ></p>', 'lxml')
+
+print head
+print css_soup.p["class"]
+print tag.name, tag.attrs
+# print soup
